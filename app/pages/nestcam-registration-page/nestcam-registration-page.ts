bba4d08d34333e4d2ae48c8a62ad2330f01601a1
Sachin Badgujar <bsachin207@gmail.com>
2016-09-15 21:11:47 -0400

    Group2 add registration page (#7)
    * Registration page added.
    
    * Alignment and validation added for reg page
    
    * Enhanced HTML page for Registration
    
    * Regestration.ts changes.
    
    * Rename Regstraionpage class.
    
    * Fixed minor errors in registration html
    
    * Fixed minor bug in ts file
    
    * added user service in ts file
    
    * class name error fixed
    
    * Made final changes
    
    * effort
    
    * changes?

--- app/pages/nestcam-registration-page/nestcam-registration-page.ts
+++ app/pages/nestcam-registration-page/nestcam-registration-page.ts
@@ -1,64 +1,55 @@
 import { Component } from '@angular/core';
 import { LoadingController, NavController } from 'ionic-angular';
-import {UserService} from '../../providers/user-service/user-service';
-import {UserModel} from '../../models/user.ts';
 import {NestCamHomePage} from '../../pages/nestcam-home/nestcam-home';
-import {FormBuilder, Validators} from '@angular/common';
+import { FormBuilder, Validators } from '@angular/forms';
+import {UserService} from '../../providers/user-service/user-service';
 
+/*
+  Generated class for the NestcamRegistrationPagePage page.
 
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
 @Component({
-  templateUrl: 'build/pages/nestcam-login-page/nestcam-login-page.html',
+  templateUrl: 'build/pages/nestcam-registration-page/nestcam-registration-page.html',
   providers: [UserService]
 })
+export class NestCamRegistrationPage {
 
-export class NestCamLoginPagePage {
-
-  loginForm: any;
-  user: UserModel;
-  errorMessage: string;
-
+  registerForm: any;
+  
   constructor(public nav: NavController, public userService: UserService, public loadingCtrl: LoadingController, private fb: FormBuilder) {
+
 
   }
 
   ionViewLoaded() {
-
-    this.loginForm = this.fb.group({
+    this.registerForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
-
   }
 
-  // Sign in current user with email and password.
-  Login(isValid: boolean) {
+  Register(isValid: boolean) {
 
     if (isValid) {
 
       // Instantiate spinner. 
       let loading = this.loadingCtrl.create({
-        content: 'Signing In...'
+        content: 'Registering In...'
       });
 
       loading.present();
 
-      this.userService.Login("", "")
-        .subscribe
-        (
-          user => this.NavigateToHomePage(user),
-          error => this.errorMessage = <any>error
-        );
-
-    }
-
-  }
-
-  NavigateToHomePage(user: UserModel),  {
       setTimeout(() => {
         loading.dismiss();
         // Navigate to Home Page after loggin in.
         this.nav.setRoot(NestCamHomePage);
       }, 3000);
+
+    }
   }
 
 }
